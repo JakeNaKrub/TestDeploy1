33e759dab99bbf2fc0ff61f98fbf25ffc4f14446
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 17:30:04 +0700

    Update Page3.py

--- Page3.py
+++ Page3.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from PIL import Image
-st.header("ตัวอย่างผลงาน")
-clicked = st.button("Click Me!")
-if clicked:
-    st.info("สร้างโดยเอกปาพู")
-    st.balloons()
-    with st.expander("Source Code!"):
-        st.write("Source Code")
-        st.write("https://github.com/JakeNaKrub/TestDeploy1")
+def app():
+    st.header("ตัวอย่างผลงาน")
+    clicked = st.button("Click Me!")
+    if clicked:
+        st.info("สร้างโดยเอกปาพู")
+        st.balloons()
+        with st.expander("Source Code!"):
+            st.write("Source Code")
+            st.write("https://github.com/JakeNaKrub/TestDeploy1")
