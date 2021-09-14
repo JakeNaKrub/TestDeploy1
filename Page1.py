8ecc059c8aa4a8150cf0e55926d1d51595e2c747
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 17:04:40 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 def app():
-    img = Image.open(r"\000003.jpg")
+    img = Image.open(r"https://github.com/JakeNaKrub/TestDeploy1/blob/main/000003.JPG")
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
 
