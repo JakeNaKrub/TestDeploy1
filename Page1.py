adc799ee68a4de99ecc0d4dc9e89edb6b81cc096
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 20:08:08 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -6,7 +6,10 @@ def loadpic(path):
     return img
  
 def app():
-    
+    a = st.button('Click Here To Go To ตัวอย่างAPI')
+    if a:
+        page = PAGES["ตัวอย่างAPI"]
+        
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
     img = loadpic("./000003.jpg")
