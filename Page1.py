9efb474282f662ad9ec8c99018da5fbe19ff50cf
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 18:02:49 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from PIL import Image
 @st.cache(suppress_st_warning=True)
-def loadpic():
-    img = Image.open("./000003.JPG")
+def loadpic(path):
+    img = Image.open(path)
     return img
  
 def app():
     
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
-    img = loadpic()
+    img = loadpic("./000003.JPG")
     st.image(img,caption="Akeprapu M4")
 
 
