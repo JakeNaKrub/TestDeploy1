fcd7ed2295186332dbcdbebbcda929cb691f03e5
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 21:38:43 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -8,7 +8,7 @@ def loadpic(path):
  
     
 def app():
-
+    st.warning("เข้าด้วยมือถืออาจกดเมนูsidebarยากนิดนึง
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
     img = loadpic("./000003.jpg")
