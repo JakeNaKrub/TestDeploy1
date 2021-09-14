3d803f46d017762cf98c8791548406d577028655
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 18:11:25 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL import Image
 @st.cache(suppress_st_warning=True,allow_output_mutation=True)
 def loadpic(path):
-    st.image(path,caption="Akeprapu M4")
- 
+    img = Image.open(path)
+    return img
  
 def app():
     
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
     img = loadpic("./000003.JPG")
- 
+    st.image(img,caption="Akeprapu M4")
 
 
