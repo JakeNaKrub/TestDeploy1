472f5f352ef492da1035a6cdeea56f61ddba600d
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 17:56:23 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -1,9 +1,14 @@
 import streamlit as st
 from PIL import Image
 @st.cache(suppress_st_warning=True)
-def app():
+def loadpic():
+    global img
     img = Image.open("./000003.JPG")
+def app():
+    
     st.title("Akeprapu's Portfolio")
     st.text("Created 08/09/2021 17:16 Made With Streamlit")
 
     st.image(img,caption="Akeprapu M4")
+loadpic()
+app()
