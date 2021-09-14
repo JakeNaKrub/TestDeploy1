dbd497299dd2c3daafc0bde2da58cbd9b2aa2657
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 17:59:38 +0700

    Update Page1.py

--- Page1.py
+++ Page1.py
@@ -5,10 +5,11 @@ def loadpic():
     img = Image.open("./000003.JPG")
     return img
  
+def app():
     
-st.title("Akeprapu's Portfolio")
-st.text("Created 08/09/2021 17:16 Made With Streamlit")
-img = loadpic()
-st.image(img,caption="Akeprapu M4")
+    st.title("Akeprapu's Portfolio")
+    st.text("Created 08/09/2021 17:16 Made With Streamlit")
+    img = loadpic()
+    st.image(img,caption="Akeprapu M4")
 
-app()
+
