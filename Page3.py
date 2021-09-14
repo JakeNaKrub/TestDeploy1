45dfbf74ccc3ab9eee52e8a78d34d5996fa35b40
JakeNaKrub <90505644+JakeNaKrub@users.noreply.github.com>
2021-09-14 18:19:49 +0700

    Update Page3.py

--- Page3.py
+++ Page3.py
@@ -5,7 +5,7 @@ def app():
     st.header("ตัวอย่างผลงาน")
     clicked = st.button("Click Me!")
     if clicked:
-        st.sucess("สร้างโดยเอกปาพู")
+        st.success("สร้างโดยเอกปาพู")
         #st.balloons()
         with st.expander("Source Code!"):
             st.write("Source Code")
